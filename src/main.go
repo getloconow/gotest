d906a5d654b65abb26d3947565b9a687ab91c88f
junaid <junaid1460@gmail.com>
2018-07-24 16:04:31 +0530

    mod

--- src/main.go
+++ src/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -11,46 +13,10 @@ var upgrader = websocket.Upgrader{}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+		w.Write([]byte("Hello"))
 	})
 
-	http.HandleFunc("/v1/ws", func(w http.ResponseWriter, r *http.Request) {
-		var conn, _ = upgrader.Upgrade(w, r, nil)
-		go func(conn *websocket.Conn) {
-			for {
-				mType, msg, _ := conn.ReadMessage()
-
-				conn.WriteMessage(mType, msg)
-			}
-		}(conn)
-	})
-
-	http.HandleFunc("/v2/ws", func(w http.ResponseWriter, r *http.Request) {
-		var conn, _ = upgrader.Upgrade(w, r, nil)
-		go func(conn *websocket.Conn) {
-			for {
-				_, msg, _ := conn.ReadMessage()
-				println(string(msg))
-			}
-		}(conn)
-	})
-
-	http.HandleFunc("/v3/ws", func(w http.ResponseWriter, r *http.Request) {
-		var conn, _ = upgrader.Upgrade(w, r, nil)
-		go func(conn *websocket.Conn) {
-			ch := time.Tick(5 * time.Second)
-
-			for range ch {
-				conn.WriteJSON(myStruct{
-					Username:  "mvansickle",
-					FirstName: "Michael",
-					LastName:  "Van Sickle",
-				})
-			}
-		}(conn)
-	})
-
-	http.HandleFunc("/v4/ws", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		var conn, _ = upgrader.Upgrade(w, r, nil)
 		go func(conn *websocket.Conn) {
 			for {
@@ -74,7 +40,8 @@ func main() {
 		}(conn)
 	})
 
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(os.Args[1], nil)
+	fmt.Println("exited", os.Args[1])
 }
 
 type myStruct struct {
