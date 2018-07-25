552d877162b29659adfb7da3cb47ed66bb28b463
junaid <junaid1460@gmail.com>
2018-07-25 18:27:24 +0530

    sfsdf

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 const ws = require('websocket').client
 let data = {}
-function test(id) {
+function test(port) {
     let start, end;
     let admin = new ws()
     admin.on('connect', function (conn) {
@@ -11,24 +11,18 @@ function test(id) {
                 time = message.utf8Data.split('|')
 
                 console.log(end - +time[1], message.utf8Data)
-                data[id] = end - +time[1]
+
             }
         });
-        var stdin = process.openStdin();
-
-        stdin.addListener("data", function (d) {
-            start = Date.now()
-            conn.sendUTF(d.toString().trim() + " | " + start.toString())
-            // d.toString().trim()
-
-        });
+        start = Date.now()
+        conn.sendUTF(port + " | " + start.toString())
+        // d.toString().trim()
     })
-    admin.connect("ws://13.127.173.119:3000/admin")
-
-
+    console.log(`ws://13.127.173.119:${port}/admin`)
+    admin.connect(`ws://13.127.173.119:${port}/admin`)
 }
 
-function tclient(id) {
+function tclient(id, port) {
     let client = new ws()
     client.on('connect', function (conn) {
         conn.on('message', function (message) {
@@ -50,26 +44,36 @@ function tclient(id) {
 
         });
     })
-    client.connect("ws://13.127.173.119:3000/")
+
+    client.connect(`ws://13.127.173.119:${port}/`)
 }
+
+ps = 30
+pps = 400
 if (process.argv[2] == 'admin') {
     console.log("admin")
-    test()
+    for (let port = 3000; port < 3000 + ps; port++)
+        test(port)
 }
 else {
     process.setMaxListeners(500)
-    for (let i = 0; i < 1000; i++) {
-        tclient(i)
+    let i = 0
+    for (let port = 3000; port < 3000 + ps; port++) {
+        let limit = i + pps
+        for (; i < limit; i++) {
+            tclient(i, port)
+        }
     }
     setInterval(function () {
         // console.log(Object.values(data))
+        console.log("-------------------")
         let vals = array(Object.values(data))
         console.log(min(vals))
         console.log(max(vals))
         console.log(sum(vals) / vals.length)
         console.log(vals.length)
-        data = {}
-    }, 3000)
+
+    }, 7000)
 
 }
 
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 
 
 for i in `seq $1 $2`;do
-    go run src/main.go 0.0.0.0:$i &
+    sleep 2 && go run src/main.go 0.0.0.0:$i &
+    
 done
